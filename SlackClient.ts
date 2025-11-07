529f37ec500528145dd2ddea078374bc0159230e
Kiran <kiran@formant.io>
2025-11-07 22:18:49 +0530

    Fix build

--- SlackClient.ts
+++ SlackClient.ts
@@ -283,4 +283,13 @@ export class SlackClient {
 
     return response.json();
   }
+
+  private handleTokenError(responseData: any): any {
+    if ((responseData as any).error === 'token_revoked') {
+      throw new Error(
+        'Your token has been revoked. Please relogin to Slack MCP integration.'
+      );
+    }
+    return responseData;
+  }
 }
